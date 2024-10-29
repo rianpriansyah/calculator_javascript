6ff61886079ca389d44b8227c57b58ea86cf08be
Rian Priansyah <rian.priansyah04@gmail.com>
2024-10-29 09:22:22 +0700

    memodifikasi kalkulator gaya iphone

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const tombol = document.querySelector(".container-tombol");
 const layar = document.querySelector("#layar");
+let resetLayar = false;
+let bolehHitung = false;
+let tmpVal = "";
+let operator = "";
 
 tombol.addEventListener("click", (e) => {
   const tombolClick = e.target.innerText;
@@ -9,8 +13,26 @@ tombol.addEventListener("click", (e) => {
   } else if (tombolClick == "<") {
     layar.value = layar.value.slice(0, -1);
   } else if (tombolClick == "=") {
-    layar.value = eval(layar.value);
+    if (bolehHitung == true) {
+      layar.value = eval(tmpVal + operator + layar.value);
+      bolehHitung = false;
+    }
+  } else if (e.target.classList.contains("operator")) {
+    if (bolehHitung == true) {
+      layar.value = eval(tmpVal + operator + layar.value);
+      bolehHitung = false;
+    }
+
+    tmpVal = layar.value;
+    operator = tombolClick;
+    resetLayar = true;
   } else {
-    layar.value += tombolClick;
+    if (resetLayar == true) {
+      layar.value = tombolClick;
+      resetLayar = false;
+      bolehHitung = true;
+    } else {
+      layar.value += tombolClick;
+    }
   }
 });
